bf79f28f8c2b4df53974375441e0f7d396a680be
Kross55 <iyakushenko84@gmail.com>
2022-09-11 14:23:17 +0300

    13-this--tasks

---
+++ main.js
@@ -0,0 +1,50 @@
+'use strict'
+
+/** 
+let calculator = {
+    // a: 5,
+    // b: 5,
+    read() {
+        this.a = +prompt('a?', 0); 
+        this.b = +prompt('b?', 0);
+    },
+    sum() {
+        return this.a + this.b
+    },
+    mul() {
+        return this.a * this.b
+    },
+};
+  
+calculator.read();
+alert( calculator.sum() );
+alert( calculator.mul() );
+
+console.log( calculator )
+*/
+
+/** 
+let ladder = {
+    step: 0,
+    up() {
+      this.step++;
+      return this;
+    },
+    down() {
+      this.step--;
+      return this;
+    },
+    showStep: function() { // показывает текущую ступеньку
+      alert( this.step );
+      return this;
+    }
+};
+
+ladder
+  .up()
+  .up()
+  .down()
+  .showStep() // 1
+  .down()
+  .showStep(); // 0
+ */ 
