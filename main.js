1999ebc482d282fee31caf983975fb1333bcc5ad
Kross55 <iyakushenko84@gmail.com>
2022-09-21 17:56:02 +0300

    25-setTimeout-&-setInterval

--- main.js
+++ main.js
@@ -1206,3 +1206,42 @@ function sum(a) {
 
 alert( sum(0)(1)(2)(3)(4)(5) )
 */
+
+// ========= setTimeout, setIntervals ==============
+/** 
+
+// 1. Вывод каждую секунду
+// важность: 5
+// Напишите функцию printNumbers(from, to), которая выводит число каждую секунду, начиная от from и заканчивая to.
+
+// Сделайте два варианта решения.
+
+// Используя setInterval.
+// Используя рекурсивный setTimeout.
+// function printNumbers(from, to) {
+//   let current = from;
+
+//   let timerId = setInterval(function() {
+//     alert(current);
+//     if (current == to) {
+//       clearInterval(timerId);
+//     }
+//     current++;
+//   }, 1000);
+// }
+
+function printNumbers(from, to) {
+  let current = from;
+  
+  setTimeout(function go() {
+    alert(current);
+    if (current < to){
+      setTimeout(go, 1000)
+    }
+    current++;
+  }, 1000);
+}
+
+// использование:
+printNumbers(5, 10);
+*/
