8f483db4f929b26f4d27432a7a65d4c48a0110cc
Kross55 <iyakushenko84@gmail.com>
2022-09-27 16:56:14 +0300

    31-codewars--task10

--- main.js
+++ main.js
@@ -1378,6 +1378,49 @@ console.log( longest(a, b) )
 //   return Array.from(new Set(s1 + s2)).sort().join('');
 // }
 
+// task 10
+// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+// If anything in the text isn't a letter, ignore it and don't return it.
+
+// "a" = 1, "b" = 2, etc.
+
+// Example
+// alphabetPosition("The sunset sets at twelve o' clock.")
+// Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
+
+let str = "The sunset sets at twelve o' clock.";
+
+function alphabetPosition(str) {
+
+  let result = [];
+
+  let arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+ 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+  'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  
+  let arr1 = str.split('');
+  
+  for(let i = 0; i < arr1.length; i++){
+    if (arr.includes(arr1[i].toLowerCase()))  result.push(+arr.indexOf(arr1[i].toLowerCase()) + 1)
+  }
+
+  return result.join(' ');
+}
+
+alert(alphabetPosition(str))
+
+// not my solution
+// function alphabetPosition(text) {
+//   var result = "";
+//   for (var i = 0; i < text.length; i++){
+//     var code = text.toUpperCase().charCodeAt(i)
+//     if (code > 64 && code < 91) result += (code - 64) + " ";
+//   }
+
+//   return result.slice(0, result.length-1);
+// }
+
 */
 
 // ==== Декораторы и переадресация вызова, call/apply ===
@@ -1681,5 +1724,6 @@ let rabbit = new Rabbit("Кроль");
 
 alert( rabbit.hasOwnProperty('name') ); // Ошибка
 */
+
 
 
